334bce131c715c88277932243a683ffb6470050c
Jemali Vashakmadze <jemali.vashakmadze1@gmail.com>
2022-05-23 10:49:02 -0700

    added index.html (#1)
    Co-authored-by: jvashakmadzeDC <jemali.vashakmadze@dresslerconsulting.com>

--- app.py
+++ app.py
@@ -13,6 +13,10 @@ def get_user(username):
     return f'<h1>Hello {username}</h1>'
 
 
+@app.route('/puppy')
+def get_puppy():
+    return render_template('index.html')
+
+
 if __name__ == '__main__':
     app.run(port=5005)
-    

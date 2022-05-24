77038d3d2494df2dc92bffa3914f15e21a8ad328
jvashakmadzeDC <jemali.vashakmadze@dresslerconsulting.com>
2022-05-23 21:16:33 -0700

    added app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def get_main():
+    return '<h1>Welcome to our flask application</h1>'
+
+
+@app.route('/<username>')
+def get_user(username):
+    return f'<h1>Hello {username}</h1>'
+
+
+if __name__ == '__main__':
+    app.run(port=5005)

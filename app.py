aba26439fb41266c2a9b28446443e452da72c447
jvashakmadzeDC <jemali.vashakmadze@dresslerconsulting.com>
2022-05-23 21:35:52 -0700

    added index.html

--- app.py
+++ app.py
@@ -13,5 +13,11 @@ def get_user(username):
     return f'<h1>Hello {username}</h1>'
 
 
+@app.route('/puppy')
+def get_puppy():
+    return render_template('index.html')
+
+
 if __name__ == '__main__':
     app.run(port=5005)
+    
